1e9e9cae67a07cbc19ad8e888cd062c2e8a0f606
Ana Recuero <anarecsa@gmail.com>
2018-01-29 18:05:52 +0100

    start and stop working

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,28 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction  = "up";
+    this.interval;
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() { 
+    this.interval = setInterval(() => this.update() ,1000)
+  }
+  stop() { 
+    clearInterval(this.interval)
+   // return;
+  }
+  update() { 
+    return this.log();
+  }
   _passengersEnter() { }
   _passengersLeave() { }
   floorUp() { }
   floorDown() { }
   call() { }
-  log() { }
+  log() { 
+    return console.log(`Direction ${this.direction} | Floor: ${this.floor}`);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,7 @@
 const Elevator = require('./elevator.js');
+
+var myElevator = new Elevator();
+
+myElevator.start();
+
+setTimeout(() => myElevator.stop(), 5000)
