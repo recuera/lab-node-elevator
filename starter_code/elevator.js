eaf7e3012be92a1f4ddd31311b1a6d8452d583f1
Ana Recuero <anarecsa@gmail.com>
2018-01-29 22:12:15 +0100

    people enter and leave the elevator

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -20,16 +20,46 @@ class Elevator {
   update() { 
     return this.log();
   }
-  _passengersEnter() { }
-  _passengersLeave() { }
+  _passengersEnter() { 
+    for (let i = 0; i < this.waitingList.length; i++){
+      if (this.floor == this.waitingList[i].originFloor){
+        this.requests.push(this.waitingList[i].destinationFloor)
+        this.passengers.push(this.waitingList[i]);
+        console.log(`${this.waitingList[i].name} has entered the elevator.`);
+        this.waitingList.splice(i, 1);
+        this.requests.splice(i, 1);
+      }
+    }
+  }
+  _passengersLeave() { 
+    for (let i = 0; i < this.passengers.length; i++){
+      if (this.floor == this.passengers[i].destinationFloor){
+        console.log(`${this.passengers[i].name} has left the elevator.`);
+        this.requests.splice(i, 1);
+        this.passengers.splice(i,1);
+      }
+    }
+  }
   floorUp() {
     if(this.floor < this.MAXFLOOR){
       this.floor++;
+      if(this.waitingList.length > 0){
+        this._passengersEnter();
+      }
+      if(this.passengers.length > 0){
+        this._passengersLeave();
+      }
     }
   }
   floorDown() { 
     if(this.floor > 0){
       this.floor--;
+      if(this.waitingList.length > 0){
+        this._passengersEnter();
+      }
+      if(this.passengers.length > 0){
+        this._passengersLeave();
+      }
     }
   }
   call(pers) {
--- starter_code/index.js
+++ starter_code/index.js
@@ -2,14 +2,23 @@ const Elevator = require('./elevator.js');
 const Person = require('./person.js');
 
 let myElevator = new Elevator();
+let myPerson = new Person("Fulanito",1,3);
+let myPerson2 = new Person("Menganito",9,5);
 
-// myElevator.floorUp();
-// myElevator.log();
-// myElevator.floorDown();
-// myElevator.log();
-// myElevator.floorDown();
-// myElevator.log();
-
-
- myElevator.start()
- setTimeout(() => myElevator.stop(), 5000)
+myElevator.call(myPerson)
+myElevator.call(myPerson2)
+myElevator.start();
+setInterval(function(){
+  if (myElevator.floor < 10 && myElevator.direction == "up"){
+    myElevator.floorUp();
+  }
+  else if (myElevator.floor > 0 && myElevator.direction == "down"){
+    myElevator.floorDown();
+  }
+  if(myElevator.floor == 10){
+    myElevator.direction = "down";
+  }
+  else if(myElevator.floor == 0){
+    myElevator.direction = "up";
+  }
+}, 1000)
